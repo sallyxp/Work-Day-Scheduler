ddcaf73633757f29f411ee0c05ec694365061cc3
Sally Rodgers <74880431+sallyxp@users.noreply.github.com>
2021-01-17 23:23:28 +0000

    Add files via upload
    Addition of project files

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# HWUnit05
-The task is to create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+# Project: Work Day Scheduler
+
+## Description 
+
+This project creates a simple calendar application to allow a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+What are the steps required to install this project? 
+Merely type in the URL listed below into the internet to access the site online as you would any other web page. 
+
+
+## Usage 
+
+This page is primarily intended to run a quiz as per the original specification.  That is to create and run multiple choice questions on Javascript, written in Javascript.
+
+
+## Credits
+
+The original specification is located withim this repository.
+
+https://birmingham.bootcampcontent.com/university-of-birmingham/ubhm-brm-fsf-pt-11-2020-u-c/tree/master/Week-05-Third-Party-APIs/02-Homework
+
+
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Sally Rodgers]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+
+## Badges
+
+
+## Tests
+
+1. Does the page open at the given location?
+2. Is the Multiple choice quiz displayed if 'Start Quiz' is clicked?
+3. Does the timer deduct 10 seconds if a question is answered incorrectly?
+4. Does the correct score display at the end of the program?
+
+---
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
---
+++ script.js
@@ -0,0 +1,129 @@
+
+//Today's date for title
+var today = document.querySelector("#currentDay");
+var momentElement = moment().format('dddd, MMMM Do');
+//populates fields
+var nineAmElement = document.querySelector("#user-input9");
+var tenAmElement = document.querySelector("#user-input10");
+var elevenAmElement = document.querySelector("#user-input11");
+var twelevePmElement = document.querySelector("#user-input12");
+var onePmElement = document.querySelector("#user-input1");
+var twoPmElement = document.querySelector("#user-input2");
+var threePmElement = document.querySelector("#user-input3");
+var fourPmElement = document.querySelector("#user-input4");
+var fivePmElement = document.querySelector("#user-input5");
+var saveBtn = document.querySelector("#save");
+
+
+//var momentElement = moment().hours(Number);
+//alert(document.querySelector("#user-input9"));
+
+
+today.innerHTML = (momentElement);
+//now use variable to get current hour.might need to parse?
+var momentElement = moment().hour();
+
+
+$(".save").on("click", function () {
+    alert("in here");
+    event.preventDefault();
+   
+    console.log(this);
+    //get values within the calendar
+ 
+    var nineElem = nineAmElement.value;
+ 
+    //    var tenElem = tenAmElement.value;
+ //   var  elevenElem = elevenAmElement.value;
+ //   var twelveElem = twelevePmElement.value;
+ //   var oneElem = onePmElement.value;
+ //   var twoElem = twoPmElement.value;
+ //   var threeELem = threePmElement.value;
+ //   var fourElem = fourPmElement.value;
+ //   var fiveElem = fivePmElement.value;
+
+
+   //set items into local storage
+    localStorage.setItem("9AM-UR", JSON.stringify(nineElem));
+ //     localStorage.setItem("10AM-UR", JSON.stringify(tenElem));
+ //   localStorage.setItem("11AM-UR", JSON.stringify(elevenElem));
+ //   localStorage.setItem("12PM-UR", JSON.stringify(twelveElem));
+ //   localStorage.setItem("1PM-UR", JSON.stringify(oneElem));
+ //   localStorage.setItem("2PM-UR", JSON.stringify(twoElem));
+  //  localStorage.setItem("3PM-UR", JSON.stringify(threeELem));
+  //  localStorage.setItem("4PM-UR", JSON.stringify(fourElem));
+  //  localStorage.setItem("5PM-UR", JSON.stringify(fiveElem));
+    
+});
+
+//function to display any text from local storage
+displayText();
+
+function displayText() {
+    //gets locally stored values and displays
+    var nineAM = JSON.parse(localStorage.getItem("9AM-UR"));
+    var tenAM = JSON.parse(localStorage.getItem("10AM-UR"));
+    var elevenAM = JSON.parse(localStorage.getItem("11AM-UR"));
+    var twelevePM = JSON.parse(localStorage.getItem("12PM-UR"));
+    var onePM = JSON.parse(localStorage.getItem("1PM-UR"));
+    var twoPM = JSON.parse(localStorage.getItem("2PM-UR"));
+    var threePM = JSON.parse(localStorage.getItem("3PM-UR"));
+    var fourPM = JSON.parse(localStorage.getItem("4PM-UR"));
+    var fivePM = JSON.parse(localStorage.getItem("5PM-UR"));
+
+ 
+//not sure about this
+    //assigns locally stored string into hour 
+    //*****NOT IN HERE */
+    nineAmElement.value = nineAM;
+    //value and local storage - jquery
+    $("#hour9 .description").val(localStorage.getItem("9AM-UR"));
+  //  tenAmElement.value = tenAM;
+  //  elevenAmElement.value = elevenAM;
+  //  twelevePmElement.value = twelevePM;
+  //  onePmElement.value = onePM;
+  //  twoPmElement.value = twoPM;
+  //  threePmElement.value = threePM;
+  //  fourPmElement.value = fourPM;
+  //  fivePmElement.value = fivePM;
+
+  function hourTracker() {
+    //get current number of hours.
+    var currentHour = moment().hour(); // use of moment.js
+
+    // loop over time blocks
+    $(".form-control").each(function () {
+        alert($(nineAmElement).attr("data-time"));
+        var blockHour = parseInt($(this).attr("id"));
+        //var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        alert(blockHour);
+        console.log( blockHour, currentHour)
+        
+
+        //check if we've moved past this time, click into css/html given classes of past, present, or future
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker(); //re-run function
+//    if (moment().format("HH") == $(nineAmElement).attr("data-time"))
+//        $(nineAmElement).css("backgroundColor", "gray");
+//    else if (moment().format("HH") > $(nineAmElement).attr("data-time"))
+//        $(nineAmElement).css("backgroundColor", "red");
+//    else
+//        $(nineAmElement).css("backgroundColor", "green");
+}
+
