c41cc89a4f59c4c427fe0473967ab4030689c98f
Sally Rodgers <74880431+sallyxp@users.noreply.github.com>
2021-01-17 23:57:42 +0000

    Add files via upload
    Added additional hours and removed some extraneous code

--- script.js
+++ script.js
@@ -14,45 +14,38 @@ var fourPmElement = document.querySelector("#user-input4");
 var fivePmElement = document.querySelector("#user-input5");
 var saveBtn = document.querySelector("#save");
 
-
-//var momentElement = moment().hours(Number);
-//alert(document.querySelector("#user-input9"));
-
-
 today.innerHTML = (momentElement);
 //now use variable to get current hour.might need to parse?
 var momentElement = moment().hour();
 
 
 $(".save").on("click", function () {
-    alert("in here");
     event.preventDefault();
    
     console.log(this);
     //get values within the calendar
  
     var nineElem = nineAmElement.value;
- 
-    //    var tenElem = tenAmElement.value;
- //   var  elevenElem = elevenAmElement.value;
- //   var twelveElem = twelevePmElement.value;
- //   var oneElem = onePmElement.value;
- //   var twoElem = twoPmElement.value;
- //   var threeELem = threePmElement.value;
- //   var fourElem = fourPmElement.value;
- //   var fiveElem = fivePmElement.value;
+    var tenElem = tenAmElement.value;
+    var  elevenElem = elevenAmElement.value;
+    var twelveElem = twelevePmElement.value;
+    var oneElem = onePmElement.value;
+    var twoElem = twoPmElement.value;
+    var threeELem = threePmElement.value;
+    var fourElem = fourPmElement.value;
+    var fiveElem = fivePmElement.value;
 
 
    //set items into local storage
     localStorage.setItem("9AM-UR", JSON.stringify(nineElem));
- //     localStorage.setItem("10AM-UR", JSON.stringify(tenElem));
- //   localStorage.setItem("11AM-UR", JSON.stringify(elevenElem));
- //   localStorage.setItem("12PM-UR", JSON.stringify(twelveElem));
- //   localStorage.setItem("1PM-UR", JSON.stringify(oneElem));
- //   localStorage.setItem("2PM-UR", JSON.stringify(twoElem));
-  //  localStorage.setItem("3PM-UR", JSON.stringify(threeELem));
-  //  localStorage.setItem("4PM-UR", JSON.stringify(fourElem));
-  //  localStorage.setItem("5PM-UR", JSON.stringify(fiveElem));
+    localStorage.setItem("10AM-UR", JSON.stringify(tenElem));
+    localStorage.setItem("11AM-UR", JSON.stringify(elevenElem));
+    localStorage.setItem("12PM-UR", JSON.stringify(twelveElem));
+    localStorage.setItem("1PM-UR", JSON.stringify(oneElem));
+    localStorage.setItem("2PM-UR", JSON.stringify(twoElem));
+    localStorage.setItem("3PM-UR", JSON.stringify(threeELem));
+    localStorage.setItem("4PM-UR", JSON.stringify(fourElem));
+    localStorage.setItem("5PM-UR", JSON.stringify(fiveElem));
     
 });
 
@@ -77,15 +70,16 @@ function displayText() {
     //*****NOT IN HERE */
     nineAmElement.value = nineAM;
     //value and local storage - jquery
-    $("#hour9 .description").val(localStorage.getItem("9AM-UR"));
-  //  tenAmElement.value = tenAM;
-  //  elevenAmElement.value = elevenAM;
-  //  twelevePmElement.value = twelevePM;
-  //  onePmElement.value = onePM;
-  //  twoPmElement.value = twoPM;
-  //  threePmElement.value = threePM;
-  //  fourPmElement.value = fourPM;
-  //  fivePmElement.value = fivePM;
+ //   $("#hour9 .description").val(localStorage.getItem("9AM-UR"));
+ //   $("#hour10 .description").val(localStorage.getItem("10AM-UR"));
+   tenAmElement.value = tenAM;
+   elevenAmElement.value = elevenAM;
+    twelevePmElement.value = twelevePM;
+    onePmElement.value = onePM;
+    twoPmElement.value = twoPM;
+    threePmElement.value = threePM;
+    fourPmElement.value = fourPM;
+    fivePmElement.value = fivePM;
 
   function hourTracker() {
     //get current number of hours.
@@ -93,10 +87,9 @@ function displayText() {
 
     // loop over time blocks
     $(".form-control").each(function () {
-        alert($(nineAmElement).attr("data-time"));
-        var blockHour = parseInt($(this).attr("id"));
-        //var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        alert(blockHour);
+      
+       var blockHour = parseInt($(this).attr("id"));
+      
         console.log( blockHour, currentHour)
         
 
@@ -118,12 +111,6 @@ function displayText() {
         }
     })
 }
-hourTracker(); //re-run function
-//    if (moment().format("HH") == $(nineAmElement).attr("data-time"))
-//        $(nineAmElement).css("backgroundColor", "gray");
-//    else if (moment().format("HH") > $(nineAmElement).attr("data-time"))
-//        $(nineAmElement).css("backgroundColor", "red");
-//    else
-//        $(nineAmElement).css("backgroundColor", "green");
+hourTracker(); 
 }
 
