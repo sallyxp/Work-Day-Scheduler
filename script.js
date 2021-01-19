7b805dbca4e267896c9b3e398b59bfe7b25a8653
Sally Rodgers <74880431+sallyxp@users.noreply.github.com>
2021-01-19 21:51:32 +0000

    Script amended to enable the colour code to change to display if the hour is before, at the same time or in the future of the present hour.

--- script.js
+++ script.js
@@ -1,116 +1,66 @@
 
-//Today's date for title
-var today = document.querySelector("#currentDay");
-var momentElement = moment().format('dddd, MMMM Do');
-//populates fields
-var nineAmElement = document.querySelector("#user-input9");
-var tenAmElement = document.querySelector("#user-input10");
-var elevenAmElement = document.querySelector("#user-input11");
-var twelevePmElement = document.querySelector("#user-input12");
-var onePmElement = document.querySelector("#user-input1");
-var twoPmElement = document.querySelector("#user-input2");
-var threePmElement = document.querySelector("#user-input3");
-var fourPmElement = document.querySelector("#user-input4");
-var fivePmElement = document.querySelector("#user-input5");
-var saveBtn = document.querySelector("#save");
-
-today.innerHTML = (momentElement);
-//now use variable to get current hour.might need to parse?
-var momentElement = moment().hour();
-
-
-$(".save").on("click", function () {
-    event.preventDefault();
+$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+   var currentHour = moment().hour();
+  //for testing purposes
+  //var currentHour = 13;
    
-    console.log(this);
-    //get values within the calendar
- 
-    var nineElem = nineAmElement.value;
-    var tenElem = tenAmElement.value;
-    var  elevenElem = elevenAmElement.value;
-    var twelveElem = twelevePmElement.value;
-    var oneElem = onePmElement.value;
-    var twoElem = twoPmElement.value;
-    var threeELem = threePmElement.value;
-    var fourElem = fourPmElement.value;
-    var fiveElem = fivePmElement.value;
 
+    //display current day & time.
+   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    //assign saveBtn click listener for user input and time stamp??
 
-   //set items into local storage
-    localStorage.setItem("9AM-UR", JSON.stringify(nineElem));
-    localStorage.setItem("10AM-UR", JSON.stringify(tenElem));
-    localStorage.setItem("11AM-UR", JSON.stringify(elevenElem));
-    localStorage.setItem("12PM-UR", JSON.stringify(twelveElem));
-    localStorage.setItem("1PM-UR", JSON.stringify(oneElem));
-    localStorage.setItem("2PM-UR", JSON.stringify(twoElem));
-    localStorage.setItem("3PM-UR", JSON.stringify(threeELem));
-    localStorage.setItem("4PM-UR", JSON.stringify(fourElem));
-    localStorage.setItem("5PM-UR", JSON.stringify(fiveElem));
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+        var blockHour = $(this).parent().attr("id");
+            
+        localStorage.setItem(time, text);
     
-});
-
-//function to display any text from local storage
-displayText();
-
-function displayText() {
-    //gets locally stored values and displays
-    var nineAM = JSON.parse(localStorage.getItem("9AM-UR"));
-    var tenAM = JSON.parse(localStorage.getItem("10AM-UR"));
-    var elevenAM = JSON.parse(localStorage.getItem("11AM-UR"));
-    var twelevePM = JSON.parse(localStorage.getItem("12PM-UR"));
-    var onePM = JSON.parse(localStorage.getItem("1PM-UR"));
-    var twoPM = JSON.parse(localStorage.getItem("2PM-UR"));
-    var threePM = JSON.parse(localStorage.getItem("3PM-UR"));
-    var fourPM = JSON.parse(localStorage.getItem("4PM-UR"));
-    var fivePM = JSON.parse(localStorage.getItem("5PM-UR"));
-
- 
-//not sure about this
-    //assigns locally stored string into hour 
-    //*****NOT IN HERE */
-    nineAmElement.value = nineAM;
-    //value and local storage - jquery
- //   $("#hour9 .description").val(localStorage.getItem("9AM-UR"));
- //   $("#hour10 .description").val(localStorage.getItem("10AM-UR"));
-   tenAmElement.value = tenAM;
-   elevenAmElement.value = elevenAM;
-    twelevePmElement.value = twelevePM;
-    onePmElement.value = onePM;
-    twoPmElement.value = twoPM;
-    threePmElement.value = threePM;
-    fourPmElement.value = fourPM;
-    fivePmElement.value = fivePM;
-
-  function hourTracker() {
-    //get current number of hours.
-    var currentHour = moment().hour(); // use of moment.js
-
-    // loop over time blocks
-    $(".form-control").each(function () {
-      
-       var blockHour = parseInt($(this).attr("id"));
-      
-        console.log( blockHour, currentHour)
-        
-
-        //check if we've moved past this time, click into css/html given classes of past, present, or future
-        if (blockHour < currentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        else if (blockHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
     })
-}
-hourTracker(); 
-}
+    //load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
+ 
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+     
+    //hour tracker colour codes the time blocks
+    
+    hourTracker();
 
+    function hourTracker() {
+        blockHour = 9;
+        // loop over time blocks
+        $(".time-block").each(function () {
+            // cycles round the time block class and checks to see whether past,present,future
+            // in relation to current hour.
+            
+            //check if we've moved past this time, click into css/html given classes of past, present, or future
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                   }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                
+            }
+            else {
+               console.log("in future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+               
+            }
+            blockHour++;
+        })
+    }
+ //   hourTracker(); //re-run function
+})
